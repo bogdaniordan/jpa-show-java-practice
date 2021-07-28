dae4066587a2850d879e53bfc28e47bd5656ca3b
bogdaniordan <bogdan.iordan47@yahoo.com>
2021-07-28 12:18:07 +0300

    Initialized project

---
+++ movies-jpa/src/main/java/com/codecool/moviesjpa/MoviesJpaApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.moviesjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesJpaApplication.class, args);
+	}
+
+}
---
+++ movies-jpa/src/test/java/com/codecool/moviesjpa/MoviesJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.moviesjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
